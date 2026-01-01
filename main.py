d191006840f3443a5a4b1fdef520762fb7bcacea
jameskelvin5119-cmd <jameskelvin5119@gmail.com>
2026-01-01 04:36:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 # =========================
 # Environment variables
-# =========================
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
